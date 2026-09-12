1c66084d1fbb0b91fb974959abd07288bca290dd
arx-ein <arxein7@gmail.com>
2026-09-12 19:02:59 +0900

    Revert "Use Viewbox"
    This reverts commit fb1f68f2d6b30829b24b483c443f6bfc9e70c2b6.

--- OpenUtau/ViewModels/NotesViewModel.cs
+++ OpenUtau/ViewModels/NotesViewModel.cs
@@ -84,7 +84,9 @@ namespace OpenUtau.App.ViewModels {
         [Reactive] public partial Bitmap? Avatar { get; set; }
         [Reactive] public partial Bitmap? Portrait { get; set; }
         [Reactive] public partial IBrush? PortraitMask { get; set; }
-        [Reactive] public partial Thickness PortraitMargin { get; set; }
+        [Reactive] public partial Rect PortraitCanvasBounds { get; set; }
+        [Reactive] public partial double PortraitHeight { get; set; }
+        [Reactive] public partial double PortraitPosition { get; set; }
         [Reactive] public partial string WindowTitle { get; set; } = "Piano Roll";
         [Reactive] public partial SolidColorBrush TrackAccentColor { get; set; } = ThemeManager.GetTrackColor("Blue").AccentColor;
         public double ViewportTicks => viewportTicks.Value;
@@ -148,6 +150,8 @@ namespace OpenUtau.App.ViewModels {
             smallChangeY = this.WhenAnyValue(x => x.ViewportTracks)
                 .Select(h => h / 8)
                 .ToProperty(this, x => x.SmallChangeY);
+            this.WhenAnyValue(x => x.PortraitCanvasBounds)
+                .Subscribe(UpdatePortraitHeight);
             this.WhenAnyValue(x => x.Bounds)
                 .Subscribe(_ => {
                     OnXZoomed(new Point(), 0);
@@ -332,7 +336,7 @@ namespace OpenUtau.App.ViewModels {
                             LoadPortrait(Part, Project);
                             break;
                         case "PortraitHeight":
-                            UpdatePortraitHeight();
+                            UpdatePortraitHeight(PortraitCanvasBounds);
                             break;
                         case "PortraitOpacity":
                             UpdatePortraitOpacity();
@@ -359,12 +363,13 @@ namespace OpenUtau.App.ViewModels {
                 });
         }
 
-        private void UpdatePortraitHeight() {
-            if (Preferences.Default.ShowPortrait && Singer != null && Portrait != null) {
-                double cap = 1 - ((double) (Singer.PortraitHeightCap > 0 ? Singer.PortraitHeightCap : Preferences.Default.PortraitHeightCap) / 100);
-                double pos = (double) (Singer.PortraitPosition > -1 ? Singer.PortraitPosition : Preferences.Default.PortraitPosition) / 100;
-                double margin = Portrait.Size.Height * cap;
-                PortraitMargin = new(0, margin * pos, 0, margin * (1 - pos));
+        private void UpdatePortraitHeight(Rect bounds) {
+            if (Preferences.Default.ShowPortrait && Singer != null) {
+                int cap = Singer.PortraitHeightCap > 0 ? Singer.PortraitHeightCap : Preferences.Default.PortraitHeightCap;
+                double heightCap = bounds.Height * cap / 100;
+                int pos = Singer.PortraitPosition > -1 ? Singer.PortraitPosition : Preferences.Default.PortraitPosition;
+                PortraitPosition = (bounds.Height - heightCap) * pos / 100;
+                PortraitHeight = heightCap;
             }
         }
 
@@ -590,7 +595,6 @@ namespace OpenUtau.App.ViewModels {
                                     Portrait = ResizePortrait(new Bitmap(stream), singer.PortraitHeight);
                                     portraitSource = singer.Portrait;
                                 }
-                                UpdatePortraitHeight();
                             }
                         } catch (Exception e) {
                             Portrait?.Dispose();
@@ -600,6 +604,7 @@ namespace OpenUtau.App.ViewModels {
                         }
                     }
                 });
+                UpdatePortraitHeight(PortraitCanvasBounds);
             }
         }
         private void LoadWindowTitle(UPart? part, UProject? project) {
